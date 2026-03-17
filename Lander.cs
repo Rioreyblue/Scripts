685f15ec914a8212f6868416d9f8e9bc7c7148f8
Rioreyblue <franciscorey702@gmail.com>
2026-03-17 23:56:32 +0800

    Add debug logs for left and right arrow key presses in Lander class

--- Lander.cs
+++ Lander.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +7,37 @@ public class Lander : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-         if (Keyboard.current.upArrowKey.isPressed)
-        {
-            Debug.Log("Up Arrow Key is Pressed");
-        }
-        if (Keyboard.current.downArrowKey.isPressed)
-        {
-            Debug.Log("Down Arrow Key is Pressed");
-        }
+        //  if (Keyboard.current.upArrowKey.isPressed)
+        // {
+        //     Debug.Log("Up Arrow Key is Pressed");
+        // }
+        // if (Keyboard.current.downArrowKey.isPressed)
+        // {
+        //     Debug.Log("Down Arrow Key is Pressed");
+        // }
+        // if (Keyboard.current.leftArrowKey.isPressed)
+        // {
+        //     Debug.Log("Left Arrow Key is Pressed");
+        // }
+        // if (Keyboard.current.rightArrowKey.isPressed)
+        // {
+        //     Debug.Log("Right Arrow Key is Pressed");
+        // }
+        
+
+        // Capture horizontal and vertical input as a single "direction"
+    Vector2 moveInput = Vector2.zero;
+
+    if (Keyboard.current.upArrowKey.isPressed)    moveInput.y = 1;
+    if (Keyboard.current.downArrowKey.isPressed)  moveInput.y = -1;
+    if (Keyboard.current.leftArrowKey.isPressed)  moveInput.x = -1;
+    if (Keyboard.current.rightArrowKey.isPressed) moveInput.x = 1;
+
+    // Now you have one variable that knows exactly where to go!
+    if (moveInput != Vector2.zero)
+    {
+        Debug.Log($"Moving in direction: {moveInput}");
+        // transform.Translate(moveInput * speed * Time.deltaTime);
+    }
     }
 }
