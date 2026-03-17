a95bd28df7f7493635d3e725bf57f5b4aca8936d
Rioreyblue <franciscorey702@gmail.com>
2026-03-18 00:14:25 +0800

    Refactor Lander class to streamline input handling and remove commented code

--- Lander.cs
+++ Lander.cs
@@ -1,43 +1,32 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
-
-    // Update is called once per frame
-    private void Update()
+    private Rigidbody2D _playerLander;
+    private void Awake()
     {
-        //  if (Keyboard.current.upArrowKey.isPressed)
-        // {
-        //     Debug.Log("Up Arrow Key is Pressed");
-        // }
-        // if (Keyboard.current.downArrowKey.isPressed)
-        // {
-        //     Debug.Log("Down Arrow Key is Pressed");
-        // }
-        // if (Keyboard.current.leftArrowKey.isPressed)
-        // {
-        //     Debug.Log("Left Arrow Key is Pressed");
-        // }
-        // if (Keyboard.current.rightArrowKey.isPressed)
-        // {
-        //     Debug.Log("Right Arrow Key is Pressed");
-        // }
-        
-
-        // Capture horizontal and vertical input as a single "direction"
-    Vector2 moveInput = Vector2.zero;
-
-    if (Keyboard.current.upArrowKey.isPressed)    moveInput.y = 1;
-    if (Keyboard.current.downArrowKey.isPressed)  moveInput.y = -1;
-    if (Keyboard.current.leftArrowKey.isPressed)  moveInput.x = -1;
-    if (Keyboard.current.rightArrowKey.isPressed) moveInput.x = 1;
-
-    // Now you have one variable that knows exactly where to go!
-    if (moveInput != Vector2.zero)
-    {
-        Debug.Log($"Moving in direction: {moveInput}");
-        // transform.Translate(moveInput * speed * Time.deltaTime);
+        _playerLander = GetComponent<Rigidbody2D>();
     }
+
+    private void Fixedupdate()
+    {
+         if (Keyboard.current.upArrowKey.isPressed)
+        {
+            _playerLander.addForce(new Vector2(0,1));
+        }
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            Debug.Log("Down Arrow Key is Pressed");
+        }
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            Debug.Log("Left Arrow Key is Pressed");
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            Debug.Log("Right Arrow Key is Pressed");
+        }
     }
 }
