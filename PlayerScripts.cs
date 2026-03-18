e3626c82d7db60fbd4d271138997f56d8a50cb5b
Rioreyblue <franciscorey702@gmail.com>
2026-03-18 22:12:59 +0800

    proper

--- PlayerScripts.cs
+++ PlayerScripts.cs
@@ -3,17 +3,32 @@ using UnityEngine.InputSystem;
 public class PlayerScripts : MonoBehaviour
 {
 
-    private Rigidbody2D _playerLander; 
+    private Rigidbody2D _mainPlayerLander; 
    private void Awake()
     {
-        _playerLander = GetComponents<Rigidbody2D>();
+        _mainPlayerLander = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            _playerLander.AddForce(Transform.up * Time.deltaTime);
+            _mainPlayerLander.AddForce(transform.up * Time.deltaTime);
+        }
+
+        // if (Keyboard.current.downArrowKey.isPressed)
+        // {
+        //     _mainPlayerLander.AddForce(Transform.down * Time.deltaTime);
+        // }
+
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            _mainPlayerLander.AddTorque(+5 * Time.deltaTime);
+        }
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            _mainPlayerLander.AddTorque(-5 * Time.deltaTime);
         }
     }
 }
